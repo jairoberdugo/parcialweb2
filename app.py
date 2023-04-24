858c114bbfcf2d676036734a6bab831f8157242c
Jairo Berdugo <jairoberdugoh@gmail.com>
2023-04-24 12:37:44 -0500

    agg consulta

--- app.py
+++ app.py
@@ -21,3 +21,25 @@ administrativo_schema= AdministrativoSchema(many=True)
 asistencia_schema = AsistenciaSchema()
 asistencia_schema = AsistenciaSchema(many=True)
 
+
+#  ----  GUARDAR A UN USUARIO ADMINISTRATIVO EN LA BASE DE DATOS -----
+@app.route("/saveuser_admin", methods=['POST'])
+def rutanueva():
+    rol = request.json['rol']
+    usuario = request.json['user']
+    clave = request.json['clave']
+    newuserad = Usuario(rol,usuario,clave)
+    fullname = request.json['fullname'] 
+    email = request.json['email']
+    tipoadmin = request.json['tipoadmin']
+    newuser = Administrativo(fullname, email,tipoadmin)
+    bd.session.add(newuserad)
+    bd.session.add(newuser)
+    bd.session.commit()     
+    return "administrativo guardado"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
